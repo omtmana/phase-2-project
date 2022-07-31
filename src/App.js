15fab8807d3b9cc1597a0089b29fb19f69224f7c
Olivia <oliviatmanalastas@gmail.com>
2022-07-31 14:27:09 -0400

    Installed react downgrade

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { CssBaseline, Grid } from '@material-ui/core'
 
-
+import Header from './Header/Header'
+import List from './List/List'
+import Map from './Map/Map'
+import PlaceDetails from './PlaceDetails/PlaceDetails'
 
 const App = () => {
    return(
       <div>
+         <CssBaseline />
+         <Header />
+         <Grid container spacing={3} style={{ width: '100%' }}>
+            <Grid item xs={12} md={4}>
+               <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+               <Map />
+            </Grid>
+         </Grid>
 
       </div>
    )
