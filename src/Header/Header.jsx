2ccda97d12956ec913f9641eeea50131c16a0354
Olivia <oliviatmanalastas@gmail.com>
2022-07-31 14:44:10 -0400

    Header style and navigation

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, ToolBar, Typography, InputBase, Box } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
+import useStyles from './styles'
+
+// Layout for the navigation Bar
 const Header = () => {
-   return(
-      <div>
+   const classes = useStyles();
 
-      </div>
+   return(
+      <AppBar position="static">
+         <ToolBar className={classes.toolbar}>
+            <Typography variant="h5" className={classes.title}>
+               Travel Advisor
+            </Typography>
+            <Box display="flex">
+               <Typography variant="h6" className={classes.title}>
+                  Explore new places
+               </Typography>
+               <Autocomplete>
+                  <div className={classes.searc}>
+                     <div className={classes.searchIcon}>
+                        <SearchIcon />
+                     </div>
+                     <InputBase placeholder="Search..." classes={{ root:classes.inputRoot, input: classes.inputInput }} />
+                  </div>
+               </Autocomplete>
+            </Box>
+         </ToolBar>
+      </AppBar>
    )
 }
 
